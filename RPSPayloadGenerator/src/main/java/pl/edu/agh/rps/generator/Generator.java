909d26695ac163ac55b5a45760e0dd78e4d31069
tkwiecien <tomasz.kwiecien1@gmail.com>
2014-05-19 23:18:13 +0200

    weka clasifiers, formatting and main classes for presentation

--- RPSPayloadGenerator/src/main/java/pl/edu/agh/rps/generator/Generator.java
+++ RPSPayloadGenerator/src/main/java/pl/edu/agh/rps/generator/Generator.java
@@ -1,86 +1,91 @@
 package pl.edu.agh.rps.generator;
 
+import java.text.DateFormat;
 import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Random;
 
 import pl.edu.agh.rps.metricsprovider.Provider;
-import pl.edu.agh.rps.metricsprovider.model.MetricValue;
 import pl.edu.agh.rps.metricsprovider.model.Resource;
 import pl.edu.agh.rps.metricsprovider.model.SupervisedSystem;
 
-
 public class Generator extends Thread {
-	
+
+	public static final int FREQUENCY = 5;
 	private static boolean VERBOSE = false;
-	private SupervisedSystem system;
-	private List<GeneratedResource> resources;
+	private final SupervisedSystem system;
+	private final List<GeneratedResource> resources;
 	private boolean isRunning = true;
+	public static Date lastReadingTime = null;
 
 	public Generator() {
 		system = new SupervisedSystem();
 		Provider.registerSystem(system);
 		resources = new LinkedList<GeneratedResource>();
 	}
-	
+
 	public List<GeneratedResource> getGeneratedResources() {
 		return resources;
 	}
-	
+
 	public void shutdown() {
 		this.isRunning = false;
 	}
-	
+
 	public static void setVerbose(boolean verbose) {
 		VERBOSE = verbose;
 	}
-	
+
 	public void addGeneratedResource(GenerationMode mode) {
 		Resource resource = new Resource(system);
 		Provider.registerResource(resource);
-		
+
 		GeneratedResource generated = new GeneratedResource(resource);
 		generated.setMode(mode);
 		resources.add(generated);
 	}
-	
-	public void addRelatedGeneratedResource(List<GeneratedResource> related) { 
+
+	public void addRelatedGeneratedResource(List<GeneratedResource> related) {
 		Resource resource = new Resource(system);
 		Provider.registerResource(resource);
-		
+
 		GeneratedResource generated = new GeneratedResource(resource);
 		generated.setMode(GenerationMode.RELATED);
 		generated.addRelated(related);
 		resources.add(generated);
 	}
-	
+
+	@Override
 	public void run() {
-	
+
+		DateFormat df = new SimpleDateFormat("HH:mm:ss:SS");
 		DecimalFormat format = new DecimalFormat("##.00");
 		while (isRunning) {
-			
+			lastReadingTime = new Date();
+			if (VERBOSE) {
+				System.out.print(df.format(lastReadingTime) + " ");
+			}
 			for (GeneratedResource res : resources) {
 				Provider.addMetricValue(res.nextValue());
 				if (VERBOSE) {
-					System.out.print(format.format(res.getCurrentValue()) + "|");
+					System.out
+							.print(format.format(res.getCurrentValue()) + "|");
 				}
 			}
-			
+
 			if (VERBOSE && !resources.isEmpty()) {
 				System.out.println("");
 			}
-			
+
 			try {
-				Thread.sleep(150);
+				Thread.sleep(FREQUENCY);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
-		
+
 	}
-	
-	
 }
